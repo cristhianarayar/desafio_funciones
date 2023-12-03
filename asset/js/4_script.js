b00d7a8557f7bd7dd423715303e32aa4d8f09443
cristhian <cristhianarayaro@gmail.com>
2023-12-02 21:44:45 -0300

    se corrige scriptttt

--- asset/js/4_script.js
+++ asset/js/4_script.js
@@ -1,47 +1,47 @@
-const mycolorblue = document.getElementById("blue");
-const mycolorred = document.getElementById("red");
-const mycolorgreen = document.getElementById("green");
-const mycoloryellow = document.getElementById("yellow");
-const color = "black";
-let booleanval = false;
+const mycolorblue = document.getElementById("blue")
+const mycolorred = document.getElementById("red")
+const mycolorgreen = document.getElementById("green")
+const mycoloryellow = document.getElementById("yellow")
+const color = "black"
+let booleanval = false
 
 
 //4_colores
 
 mycolorblue.addEventListener("click", () => {
   if (booleanval == false) {
-    mycolorblue.style.backgroundColor = color;
-    booleanval = true;
+    mycolorblue.style.backgroundColor = color
+    booleanval = true
   } else if (booleanval === true) {
-    mycolorblue.style.backgroundColor = "blue";
-    booleanval = false;
+    mycolorblue.style.backgroundColor = "blue"
+    booleanval = false
   }
 });
 mycolorred.addEventListener("click", () => {
   if (booleanval == false) {
-    mycolorred.style.backgroundColor = color;
-    booleanval = true;
+    mycolorred.style.backgroundColor = color
+    booleanval = true
   } else if (booleanval === true) {
-    mycolorred.style.backgroundColor = "red";
-    booleanval = false;
+    mycolorred.style.backgroundColor = "red"
+    booleanval = false
   }
 });
 mycolorgreen.addEventListener("click", () => {
   if (booleanval == false) {
-    mycolorgreen.style.backgroundColor = color;
-    booleanval = true;
+    mycolorgreen.style.backgroundColor = color
+    booleanval = true
   } else if (booleanval === true) {
-    mycolorgreen.style.backgroundColor = "green";
-    booleanval = false;
+    mycolorgreen.style.backgroundColor = "green"
+    booleanval = false
   }
 });
 mycoloryellow.addEventListener("click", () => {
   if (booleanval == false) {
-    mycoloryellow.style.backgroundColor = color;
-    booleanval = true;
+    mycoloryellow.style.backgroundColor = color
+    booleanval = true
   } else if (booleanval === true) {
-    mycoloryellow.style.backgroundColor = "yellow";
-    booleanval = false;
+    mycoloryellow.style.backgroundColor = "yellow"
+    booleanval = false
   }
 });
 
--- asset/js/5_script.js
+++ asset/js/5_script.js
@@ -1,35 +1,35 @@
-const a = "pink";
-const d = "lightblue";
-const s = "orange";
-const q = "purple";
-const w = "grey";
-const e = "brown";
-const mykey = document.getElementById("key");
-const mykey2 = document.getElementById("key2");
-const myflex = document.getElementsByClassName("flex");
+const a = "pink"
+const d = "lightblue"
+const s = "orange"
+const q = "purple"
+const w = "grey"
+const e = "brown"
+const mykey = document.getElementById("key")
+const mykey2 = document.getElementById("key2")
+const myflex = document.getElementsByClassName("flex")
 
 function addElement(color) {
-    mykey2.classList.add("div-key");
-    mykey2.style.backgroundColor = color;
+    mykey2.classList.add("div-key")
+    mykey2.style.backgroundColor = color
   }
   
-  document.addEventListener("keydown", function (event) {
+  document.addEventListener("keydown", (event) =>{
     if (event.key == "a") {
-      mykey.style.backgroundColor = a;
+      mykey.style.backgroundColor = a
     } else if (event.key == "s") {
-      mykey.style.backgroundColor = s;
+      mykey.style.backgroundColor = s
     } else if (event.key == "d") {
-      mykey.style.backgroundColor = d;
+      mykey.style.backgroundColor = d
     }
-  });
+  })
   
-  document.addEventListener("keydown", function (event) {
+  document.addEventListener("keydown", (event) => {
     if (event.key == "q") {
-      addElement(q);
+      addElement(q)
     } else if (event.key == "w") {
-      addElement(w);
+      addElement(w)
     } else if (event.key == "e") {
-      addElement(e);
+      addElement(e)
     }
-  });
+  })
   
